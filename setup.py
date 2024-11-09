522b4523d78501f2dd1a63c1a4f26d48cb8e51e1
brainybeam <159916958+brainybeam@users.noreply.github.com>
2024-11-09 21:02:13 +0100

    Added new files to make it pip installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='dsss_homework__2',
+   version='1.0',
+   description='Mathematics quiz',
+   author='Mitesh Kumar',
+   author_email='mitesh.kumar@fau.de',
+   packages=['math_quiz'],  
+   install_requires=['numpy','pandas'], 
+)
